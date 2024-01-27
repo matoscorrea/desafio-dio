a3a2656f820795d4fb662692e90b52116a676eb1
matoscorrea <dev.from.scratch@gmail.com>
2024-01-27 14:25:47 -0300

    terceiro desafio dio

---
+++ desafio3.js
@@ -0,0 +1,32 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+    switch (this.tipo) {
+      case "mago":
+        ataque = "usou magia";
+        break;
+      case "guerreiro":
+        ataque = "usou espada";
+        break;
+      case "monge":
+        ataque = "usou artes marciais";
+        break;
+      case "ninja":
+        ataque = "usou shuriken";
+        break;
+      default:
+        ataque = "atacou";
+    }
+    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+// Exemplo de uso da classe
+let heroi = new Heroi("Gandalf", 200, "mago");
+heroi.atacar();
